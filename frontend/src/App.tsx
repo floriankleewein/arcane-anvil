37c12d4f54f0e1d864eb52bdb2163afe686448ab
Florian Kleewein <florian.kleewein@gmx.at>
2025-09-06 16:04:07 +0200

    Add overall gradient background

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import ItemPage from "./Pages/ItemPage"
 function App() {
   return (
     // <div className="flex h-screen justify-center items-center select-none">
-    <div>
+    <div className="flex flex-col min-h-screen bg-gradient-to-b from-[#FFFDed] via-[#F8F5E0] to-[#F0EAD5]">
       <HeaderBar></HeaderBar>
       <ItemPage></ItemPage>
     </div>
