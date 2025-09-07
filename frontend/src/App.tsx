e5a1c527e6e79176b15605fa1e1abcbd333015d6
Florian Kleewein <florian.kleewein@gmx.at>
2025-09-07 08:16:07 +0200

    Add gap between header and page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,7 @@ import ItemPage from "./Pages/ItemPage"
 function App() {
   return (
     //bg-gradient-to-b from-[#FFFDed] via-[#F8F5E0] to-[#F0EAD5]
-    <div className="flex flex-col min-h-screen bg-slate-100">
+    <div className="flex flex-col gap-4 min-h-screen bg-slate-100">
       <HeaderBar></HeaderBar>
       <ItemPage></ItemPage>
     </div>
