f98521d4bdae4bb465e5cfd83930b5db9bbde0f1
Florian Kleewein <florian.kleewein@gmx.at>
2025-09-12 21:12:11 +0200

    Add footerbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
+import FooterBar from "./components/footerbar"
 import HeaderBar from "./components/headerbar"
 import ItemPage from "./Pages/ItemPage"
 
 function App() {
   return (
     //bg-gradient-to-b from-[#FFFDed] via-[#F8F5E0] to-[#F0EAD5]
-    <div className="flex flex-col gap-4 min-h-screen bg-slate-100">
-      <HeaderBar></HeaderBar>
-      <ItemPage></ItemPage>
+    <div className="flex flex-col gap-4 min-h-screen select-none bg-slate-100">
+      <HeaderBar />
+      <ItemPage />
+      <FooterBar />
     </div>
   )
 }
---
+++ frontend/src/components/footerbar.tsx
@@ -0,0 +1,9 @@
+export default function FooterBar() {
+  return (
+    <div className="custom-footerbar flex flex-row items-center justify-evenly p-2">
+      <span className="text-stone-100">Guide</span>
+      <span className="text-stone-100">Support me</span>
+      <span className="text-stone-100">About</span>
+    </div>
+  )
+}
